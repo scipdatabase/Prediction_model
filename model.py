7eef4e2b04711db7e5a2f24b391c183a36673c86
scipdatabase <124155816+scipdatabase@users.noreply.github.com>
2024-04-10 21:59:19 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,19 +1,15 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[49]:
-
-
-#import libraries
+# import libraries
 import pandas as pd
 import numpy as np
 from keras.models import model_from_json
 from sklearn.preprocessing import LabelEncoder
 import joblib  # To save and load LabelEncoder
-
+import sys
 
 # Model upload
-
 
 # load json and create model
 json_file = open('model_elu.json', 'r')
@@ -28,22 +24,18 @@ le = joblib.load('label_encoder.pkl')
 
 # Classification
 
-
 def preprocess_categorical_features(data, le):
     # Assuming 'Parameter' is a categorical feature in your dataset
     data['Parameter'] = le.transform(data['Parameter'])
     return data
 
-
-#Load the data for making predictions with the model 
-X_test1 = pd.read_csv('example.csv')
-
-
-X_test1.drop(X_test1.columns[[0]],axis=1, inplace=True)
+# Load the data for making predictions with the model
+filename = sys.argv[1]  # Assuming the filename is the first command line argument
+X_test1 = pd.read_csv(filename)
+X_test1.drop(X_test1.columns[[0]], axis=1, inplace=True)
 
 
 # Prediction
-
 
 predictions = loaded_model.predict(X_test1)
 
@@ -56,9 +48,5 @@ df_percent = pd.DataFrame(percent, columns=['Percentage_loss'])
 df_class = pd.DataFrame(predicted_class_labels, columns=['Class'])
 df_combined = pd.concat([df_percent, df_class], axis=1)
 
-#save the output
+# Save the output
 df_combined.to_csv('prediction_output.csv', index=False)
-
-
-
-
